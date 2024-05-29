9cfec265a9b5e080e97cba146a83d64c099740c3
shrey890 <shrey90@proton.me>
2024-05-29 12:02:37 +0530

    description

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,14 @@
 import Image from 'next/image'
 const Home = () => {
   return (
-    <section className='p-2 sm:p-5'>
-      <div className='md:flex justify-center  w-full  max-h-screen '>
+    <section className='min-h-screen p-2 sm:p-5'>
+      <div className='md:flex justify-center  w-full'>
         <div className="md:w-1/2">
           <Image src='/looking.svg' alt='looking' width={ 550 } height={ 550 } />
         </div>
         <div className="md:w-1/2">
           <h2 className='text-2xl  md:mt-7 md:text-3xl font-semibold'>Looking for more customers for <br /> your business?</h2>
-          <p></p>
+          <p>hello</p>
         </div>
       </div>
     </section>
